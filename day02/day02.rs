db55f2a34fcc038fcfe0073c4e9a3e1d4e78a533
tuxxi <aidan.sojourner@gmail.com>
2018-12-03 19:03:41 -0800

    Day two.
    Needs a lot of work, I have no idea what I'm doing! Lots of functional and std library things that could make this much cleaner

--- day02/day02.rs
+++ day02/day02.rs
@@ -1,5 +1,8 @@
 use std::fs::File;
 use std::io::prelude::*;
+use std::collections::{HashSet};
+
+const ALPHABET: &str = "abcedfghijklmnopqrstuvwxyz";
 
 fn main() {
     let filename = "day02/day02_input";
@@ -11,9 +14,87 @@ fn main() {
 
     let ids: Vec<&str> = contents.split("\n").collect();
 
-    day_one(&ids);
+    let checksum = part_one(&ids);
+    println!("checksum: {}", checksum);
+    let common = part_two(&ids);
+    println!("common: {}", common);
 }
 
-fn day_one(ids: &Vec<&str>) {
-    
+/// calculates a 'checksum' of the input strings
+fn part_one(ids: &Vec<&str>) -> i32 {
+    let mut num_twos = 0;
+    let mut num_threes = 0;
+    for &id in ids {
+        let mut counts: HashSet<u32> = HashSet::new();
+
+        for letter in ALPHABET.chars() {
+            let num = id.matches(letter).count() as u32;
+            counts.insert(num);
+        }
+        //only count one of each 2 or 3 letter recurrence
+        num_twos += counts.contains(&2) as i32;
+        num_threes += counts.contains(&3) as i32;
+    }
+    return num_twos * num_threes;
+}
+
+fn part_two(ids: &Vec<&str>) -> String {
+
+    let mut result:(&str, &str) = ("", "");
+    let mut max_commons = 0;
+    for &id in ids {
+        for &other in ids {
+            if *other != *id {
+                let mut num_common = 0;
+                for (c1, c2) in id.chars().zip(other.chars()) {
+                    if c1 == c2 {
+                        num_common += 1;
+                    }
+                    if num_common > max_commons {
+                        max_commons = num_common;
+                        result = (id, other);
+                    }
+                }
+            }
+        }
+    }
+    println!("Max chars in common: {}", max_commons);
+    println!("between IDs: {} and {}", result.0, result.1);
+
+    let mut intersection = String::new();
+    for (c1, c2) in result.0.chars().zip(result.1.chars()) {
+        if c1 == c2 {
+            intersection += &c1.to_string();
+        }
+    }
+    return intersection;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let items = vec!["abcdef",
+                     "bababc",
+                     "abbcde",
+                     "abcccd",
+                     "aabcdd",
+                     "abcdee",
+                     "ababab"];
+        assert_eq!(part_one(&items), 12)
+    }
+
+    #[test]
+    fn test_part_two() {
+        let items = vec![
+                     "abcde",
+                     "fghij",
+                     "klmno",
+                     "pqrst",
+                     "fguij",
+                     "axcye"];
+        assert_eq!(part_two(&items), "fgij".to_owned());
+    }
 }
